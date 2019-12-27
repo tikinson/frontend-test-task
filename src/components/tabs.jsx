38cabe89d98d770926ee1f4530931ca03d205e47
tikinson <tikintikitn@gmail.com>
2019-12-27 05:10:00 +0300

    add tabs

--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -3,6 +3,12 @@ import './tabs.css';
 
 const Tabs = () => {
 	return <div className="tabs"> 
+		<div className='cheap'>
+		самый дешевый
+		</div>
+		<div className='fast'>
+		самый быстрый
+		</div>
     </div>
 }
 
